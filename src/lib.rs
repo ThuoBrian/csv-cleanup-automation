489920f8f5092a8faca61e067c39a3830bae4f98
Brian Thuo <thuogachau@gmail.com>
2025-10-14 14:07:26 +0300

    refactor: Update CSV file handling to use directory path and improve column constants

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,24 @@ use polars::prelude::*;
 use std::fs::File;
 use std::path::Path;
 
+//csv column names
+pub const COL_NAME: &str = "Name";
+pub const COL_TOTAL_PRINTS: &str = "Total Prints";
+pub const COL_BW_PRINTER: &str = "Black & WhiteTotal(Printer)";
+pub const COL_BW_COPIER: &str = "Black & WhiteTotal(Copier/Document Server)";
+pub const COL_BW_LARGE: &str = "Black & White(Large size)(Copier/Document Server)";
+
 // CSV file paths under the ./data folder.
-pub const INPUT_CSV_FILE: &str = "./data/{raw-csv-doc}.csv";
+// How can we make this more dynamic to handle multiple files? [glob pattern matching?]
+pub const INPUT_CSV_DIR: &str = "./data/IPAK_NRB_PROGRAMS_.csv";
 pub const OUTPUT_CSV_FILE: &str = "./data/analyzed_output.csv";
 
 pub fn process_csv_file(input_path: &Path) -> Result<DataFrame, PolarsError> {
     // Check if the input file exists
     if !input_path.exists() {
-        eprintln!("Input file does not exist: {:?}", input_path);
+        return Err(PolarsError::ComputeError(
+            format!("Input file does not exist: {:?}", input_path).into(),
+        ));
     }
     // Open the input file
     let input_file = File::open(input_path).map_err(|error| {
@@ -29,21 +39,21 @@ pub fn process_csv_file(input_path: &Path) -> Result<DataFrame, PolarsError> {
 
     // Select specific columns
     let selected_dataframe = dataframe.select([
-        "Name",
-        "Total Prints",
-        "Black & WhiteTotal(Printer)",
-        "Black & WhiteTotal(Copier/Document Server)",
-        "Black & White(Large size)(Copier/Document Server)",
+        COL_NAME,
+        COL_TOTAL_PRINTS,
+        COL_BW_PRINTER,
+        COL_BW_COPIER,
+        COL_BW_LARGE,
     ])?;
 
     // Clean the "Name" column by removing square brackets REGEX (r#"[\[\]]"#))
     let cleaned_dataframe = selected_dataframe
         .lazy()
         .with_column(
-            col("Name")
+            col(COL_NAME)
                 .str()
                 .replace_all(lit(r#"[\[\]]"#), lit(""), false)
-                .alias("Name"),
+                .alias(COL_NAME),
         )
         .collect()?;
 
